beab8f2646d9895da8f32b45e4988b93e1f4c28f
Karol Sobczak <Karol.Sobczak@Teradata.com>
2015-03-11 14:04:27 +0100

    requirements collector: A RequirementsCollector implementation
    Summary:
    RequirementsCollector list requirements from method and delcaring class
    and returns all sets of requirements for a given test
    
    Task: HADAPT-91
    Review Url: @@review_url@@
    
    Test Plan: unit tests
    
    Reviewers: lo186005
    
    Differential Revision: http://sdvl3bdch001.td.teradata.com/D2

--- src/main/java/com/teradata/test/RequirementsCollector.java
+++ src/main/java/com/teradata/test/RequirementsCollector.java
@@ -3,16 +3,49 @@
  */
 package com.teradata.test;
 
+import com.google.common.collect.Lists;
+
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.teradata.test.Requirements.compose;
 
 /**
  * This class gathers requirements for a given test method.
  */
 public final class RequirementsCollector {
 
-    public static Set<Set<? extends Requirement>> collectRequirementsFor(Method method) {
-        return null;
+    public static Set<Set<Requirement>> collectRequirementsFor(Method method) {
+        Requires methodRequiresAnnotation = method.getAnnotation(Requires.class);
+        CompositeRequirement methodCompositeRequirement = getCompositeRequirement(methodRequiresAnnotation);
+
+        Requires classRequiresAnnotation = method.getDeclaringClass().getAnnotation(Requires.class);
+        CompositeRequirement classCompositeRequirement = getCompositeRequirement(classRequiresAnnotation);
+
+        return compose(methodCompositeRequirement, classCompositeRequirement).getRequirementsSets();
+    }
+
+    private static CompositeRequirement getCompositeRequirement(Requires requires) {
+        if (requires != null) {
+            checkArgument(requires.value() != null);
+            return compose(toRequirements(requires.value()));
+        } else {
+            return compose();
+        }
+    }
+
+    private static List<Requirement> toRequirements(Class<? extends RequirementsProvider>[] providers) {
+        return Lists.transform(Arrays.asList(providers), (Class<? extends RequirementsProvider> providerClass) -> {
+            try {
+                RequirementsProvider provider = providerClass.newInstance();
+                return provider.getRequirements();
+            } catch (InstantiationException | IllegalAccessException e) {
+                throw new IllegalArgumentException("Could not instantiate provider class", e);
+            }
+        });
     }
 
     private RequirementsCollector() {
--- src/test/groovy/com/teradata/test/RequirementsCollectorTest.groovy
+++ src/test/groovy/com/teradata/test/RequirementsCollectorTest.groovy
@@ -8,21 +8,35 @@ import spock.lang.Specification
 
 import static com.teradata.test.Requirements.allOf
 import static com.teradata.test.Requirements.compose
+import static com.teradata.test.RequirementsCollector.collectRequirementsFor
 import static java.util.Arrays.asList
 
-public class RequirementsTest extends Specification {
+public class RequirementsCollectorTest extends Specification {
 
     private static final def A = req('a')
     private static final def B = req('b')
     private static final def C = req('c')
     private static final def D = req('d')
 
-    def "test"() {
+    def "should list method requirements"() {
+        expect:
+        collectRequirementsFor(method) == expectedRequirementSets
+
+        where:
+        method                                        | expectedRequirementSets
+        MethodRequirement.getMethod('method')         | setOf(setOf(A))
+        ClassRequirement.getMethod('method')          | setOf(setOf(B))
+        MethodAndClassRequirement.getMethod('method') | setOf(setOf(A, B))
+    }
+
+    def "should compose requirements"() {
         expect:
         ((CompositeRequirement) requirement).requirementsSets == expectedRequirementSets
 
         where:
         requirement                         | expectedRequirementSets
+        compose()                           | setOf(setOf())
+
         compose(A, B)                       | setOf(setOf(A, B))
 
         allOf(A, B)                         | setOf(setOf(A), setOf(B))
@@ -37,6 +51,10 @@ public class RequirementsTest extends Specification {
 
     }
 
+    private static Requirement req(String name) {
+        return new DummyTestRequirement(name)
+    }
+
     private static <E> Set<E> setOf(E e) {
         ImmutableSet.of(e)
     }
@@ -45,8 +63,36 @@ public class RequirementsTest extends Specification {
         ImmutableSet.builder().addAll(asList(elems)).build()
     }
 
-    private static Requirement req(String name) {
-        return new DummyTestRequirement(name)
+    private static class MethodRequirement {
+        @Requires(ProviderA)
+        public void method() {
+        }
+    }
+
+    @Requires(ProviderB)
+    private static class ClassRequirement {
+        public void method() {
+        }
+    }
+
+    @Requires(ProviderA)
+    private static class MethodAndClassRequirement {
+        @Requires(ProviderB)
+        public void method() {
+        }
+    }
+
+    private static class ProviderA implements RequirementsProvider {
+        Requirement getRequirements() {
+            return A
+        }
+    }
+
+    private static class ProviderB implements RequirementsProvider {
+        @Override
+        Requirement getRequirements() {
+            return B
+        }
     }
 
     private static final class DummyTestRequirement implements Requirement {
