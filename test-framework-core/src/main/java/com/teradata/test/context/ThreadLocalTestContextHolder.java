50ddbf65724b1d03de2275f00dae0c21ace91412
Lukasz Osipiuk <losipiuk@hadapt.com>
2015-03-13 14:47:21 +0100

    Make ThreadLocalTestContextHolder final and not custructable

--- test-framework-core/src/main/java/com/teradata/test/context/ThreadLocalTestContextHolder.java
+++ test-framework-core/src/main/java/com/teradata/test/context/ThreadLocalTestContextHolder.java
@@ -8,32 +8,38 @@ import static com.google.common.base.Preconditions.checkState;
 
 /**
  * Static helper for holding TestContext in thread local variable.
- *
+ * <p>
  * Justification for existence:
- *
+ * <p>
  * Using thread local for holding current TestContext is less explicit
  * and a bit hacky. But allows for having less verbose test building blocks.
- *
+ * <p>
  * We also do not require users to subclass a common context-aware test class
  * when they write tests.
  */
-public class ThreadLocalTestContextHolder
+public final class ThreadLocalTestContextHolder
 {
     private static ThreadLocal<TestContext> testContextThreadLocal = new ThreadLocal<>();
 
-    public static void assertTestContextNotSet() {
+    private ThreadLocalTestContextHolder() {}
+
+    public static void assertTestContextNotSet()
+    {
         checkState(testContextThreadLocal.get() == null);
     }
 
-    public static void clearTestContext() {
+    public static void clearTestContext()
+    {
         testContextThreadLocal.set(null);
     }
 
-    public static void setTestContext(TestContext testContext) {
+    public static void setTestContext(TestContext testContext)
+    {
         testContextThreadLocal.set(testContext);
     }
 
-    public static TestContext testContext() {
+    public static TestContext testContext()
+    {
         TestContext testContext = testContextThreadLocal.get();
         checkState(testContext != null, "test context not set for current thread");
         return testContext;
