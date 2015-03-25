77f2abd5adc53caeb1a451bd3330df737db90661
Stanislaw Ogorkis <stanislaw.ogorkis@teradata.com>
2015-03-25 14:59:36 +0100

    Unit tests for FileParser and SqlResultFileWrapper.
    Summary:
    Added unit tests for FileParser and SqlResultFileWrapper.
    
    Tasks: SWARM-179
    
    Test Plan: Unit tests.
    
    Reviewers: ksobczak, losipiuk
    
    Subscribers: wbiela
    
    Differential Revision: https://phabricator.td.teradata.com/D76

---
+++ test-framework-core/src/test/groovy/com/teradata/test/convention/FileParserTest.groovy
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2013-2015, Teradata, Inc. All rights reserved.
+ */
+
+package com.teradata.test.convention
+
+import spock.lang.Specification
+
+import static org.apache.commons.io.IOUtils.toInputStream
+
+class FileParserTest
+        extends Specification
+{
+  private FileParser fileParser = new FileParser()
+
+  def 'parse file with comment properties'()
+  {
+    String fileContent = '-- property1: value1, property2: value2\n' +
+            'content line 1\n' +
+            'content line 2'
+    FileParser.ParsingResult parsingResult = fileParser.parseFile(toInputStream(fileContent))
+
+    expect:
+    parsingResult.getProperty("property1").get() == "value1"
+    parsingResult.getProperty("property2").get() == "value2"
+    !parsingResult.getProperty("unknownProperty").isPresent()
+    parsingResult.getContentLines().size() == 2
+    parsingResult.getContentLines().get(0) == "content line 1"
+    parsingResult.getContentLines().get(1) == "content line 2"
+    parsingResult.getContent() == "content line 1 content line 2"
+  }
+
+  def 'parse file no comment properties'()
+  {
+    String fileContent = 'content line 1\n' +
+            'content line 2'
+    FileParser.ParsingResult parsingResult = fileParser.parseFile(toInputStream(fileContent))
+
+    expect:
+    !parsingResult.getProperty("unknownProperty").isPresent()
+    parsingResult.getContentLines().size() == 2
+    parsingResult.getContentLines().get(0) == "content line 1"
+    parsingResult.getContentLines().get(1) == "content line 2"
+    parsingResult.getContent() == "content line 1 content line 2"
+  }
+}
---
+++ test-framework-core/src/test/groovy/com/teradata/test/convention/SqlResultFileWrapperTest.groovy
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2013-2015, Teradata, Inc. All rights reserved.
+ */
+
+package com.teradata.test.convention
+
+import spock.lang.Specification
+
+import java.sql.Date
+import java.sql.Time
+import java.sql.Timestamp
+
+import static java.sql.JDBCType.*
+import static org.apache.commons.io.IOUtils.toInputStream
+
+class SqlResultFileWrapperTest
+        extends Specification
+{
+
+  def 'sampleResultFile'()
+  {
+    setup:
+    String fileContent = '-- delimiter: |, ignoreOrder: true, types: VARCHAR|BINARY|BIT|INTEGER|REAL|NUMERIC|DATE|TIME|TIMESTAMP\n' +
+            'A|true|1|10|20.0|30.0|2015-11-01|10:55:25|2016-11-01 10:55:25|\n' +
+            'B|true|1|10|20.0|30.0|2015-11-01|10:55:25|2016-11-01 10:55:25|'
+    FileParser.ParsingResult parsingResult = new FileParser().parseFile(toInputStream(fileContent))
+    SqlResultFileWrapper resultFileWrapper = new SqlResultFileWrapper(parsingResult)
+
+    expect:
+    resultFileWrapper.isIgnoreOrder()
+    resultFileWrapper.getTypes() == [VARCHAR, BINARY, BIT, INTEGER, REAL, NUMERIC, DATE, TIME, TIMESTAMP]
+    resultFileWrapper.getRows().size() == 2
+    resultFileWrapper.getRows().get(0).getValues() == ['A', true, true, 10, Double.valueOf(20.0), new BigDecimal("30.0"), Date.valueOf('2015-11-01'),
+                                                       Time.valueOf('10:55:25'), Timestamp.valueOf('2016-11-01 10:55:25')]
+  }
+}
