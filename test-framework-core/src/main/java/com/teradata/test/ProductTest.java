b6ecc2d7029523d13096d08666c02e70185e95b2
Lukasz Osipiuk <losipiuk@hadapt.com>
2015-03-26 14:12:23 +0100

    Add logger definition to ProductTest
    Summary: Issue: SWARM-74
    
    Test Plan: Build
    
    Reviewers: ksobczak
    
    Differential Revision: https://phabricator.td.teradata.com/D87

--- test-framework-core/src/main/java/com/teradata/test/ProductTest.java
+++ test-framework-core/src/main/java/com/teradata/test/ProductTest.java
@@ -5,10 +5,12 @@
 package com.teradata.test;
 
 import com.teradata.test.initialization.TestInitializationListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Listeners;
 
 @Listeners(TestInitializationListener.class)
 public class ProductTest
 {
-
+    protected static final Logger LOGGER = LoggerFactory.getLogger(ProductTest.class);
 }
