caa0f5af316147d43a16be35cbae7fb1af27bf7d
Anton Petrov <anton.petrov@teradata.com>
2015-04-07 18:22:33 -0400

    python dist: commit the initial draft of the setup.py script
    * To package the python scripts into a wheel execute:
      python setup.py bdist_wheel
    * Remove src dir
    
    Task: SWARM-268
    Review Url: None
    Reviewed By: None
    Testing: manual execution of command

---
+++ test-framework-launch-script/setup.py
@@ -0,0 +1,18 @@
+#
+# Copyright 2015, Teradata, Inc. All rights reserved.
+#
+
+from setuptools import setup
+
+setup(
+    name='product-test',
+    version='1.0',
+    description='product-test will run your product tests using a custom SQL on Hadoop test harness.',
+    author="Teradata Coporation",
+    author_email='anton.petrov@teradata.com',
+    url='https://github.com/teradatalabs/test-framework',
+    packages=['argparse_extensions'],
+    py_modules=['test_common', 'test_runner', 'test_runner_argument_builder', 'test_runner_parser'],
+    keywords=['sql', 'hadoop', 'test-framework'],
+    entry_points={'console_scripts': ['product-test = test_runner:main']}
+)
