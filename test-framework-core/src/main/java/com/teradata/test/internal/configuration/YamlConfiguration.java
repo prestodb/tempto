252e41bd6bfdca5dcee691fec09274e2acea8309
Grzegorz Kokosiński <g.kokosinski@gmail.com>
2015-04-24 14:13:49 +0200

    core: add posibility to use configuration variables within configuration
    Thanks to this patch it is possible to prevent repetition in
    configuration. Consider following example:
    
    service1: server:port1
    service2: server:port2
    
    Now you can write:
    
    host: server
    service1:
    {host}:port1
    service2:
    {host}:port2
    
    Test Plan: unit tests
    
    Reviewers: ksobczak, losipiuk, sogorkis
    
    Differential Revision: https://phabricator.td.teradata.com/D362

--- test-framework-core/src/main/java/com/teradata/test/internal/configuration/YamlConfiguration.java
+++ test-framework-core/src/main/java/com/teradata/test/internal/configuration/YamlConfiguration.java
@@ -4,6 +4,7 @@
 
 package com.teradata.test.internal.configuration;
 
+import com.google.common.collect.ImmutableMap;
 import com.teradata.test.configuration.Configuration;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.text.StrSubstitutor;
@@ -31,11 +32,51 @@ public class YamlConfiguration
 
     public YamlConfiguration(InputStream yamlInputStream)
     {
-        String yamlString = ENV_SUBSTITUTOR.replace(inputStreamToStringSafe(yamlInputStream));
+        String yamlString = replaceEnvVariables(yamlInputStream);
+        yamlString = replaceConfigurationVariables(yamlString);
+        mapConfiguration = loadConfiguration(yamlString);
+    }
+
+    private String replaceEnvVariables(InputStream yamlInputStream)
+    {
+        return ENV_SUBSTITUTOR.replace(inputStreamToStringSafe(yamlInputStream));
+    }
+
+    private String replaceConfigurationVariables(String yamlString)
+    {
+        while (true) {
+            // TODO solve circular variable references
+            MapConfiguration temporaryConfiguration = loadConfiguration(yamlString);
+            StrSubstitutor variableSubstitutor = new StrSubstitutor(toMap(temporaryConfiguration));
+            String yamlStringTemporary = variableSubstitutor.replace(yamlString);
+            if (yamlString.equals(yamlStringTemporary)) {
+                break;
+            }
+            else {
+                yamlString = yamlStringTemporary;
+            }
+        }
+        return yamlString;
+    }
+
+    private MapConfiguration loadConfiguration(String yamlString)
+    {
         Yaml yaml = new Yaml();
         Object loadedYaml = yaml.load(yamlString);
         checkArgument(loadedYaml instanceof Map, "yaml does not evaluate to map object; got %s", loadedYaml.getClass().getName());
-        mapConfiguration = new MapConfiguration((Map<String, Object>) loadedYaml);
+        Map<String, Object> loadedYamlMap = (Map<String, Object>) loadedYaml;
+        return new MapConfiguration(loadedYamlMap);
+    }
+
+    private Map<String, Object> toMap(MapConfiguration configuration)
+    {
+        ImmutableMap.Builder<String, Object> mapBuilder = ImmutableMap.<String, Object>builder();
+
+        for (String key : configuration.listKeys()) {
+            mapBuilder.put(key, configuration.get(key).get());
+        }
+
+        return mapBuilder.build();
     }
 
     @Override
--- test-framework-core/src/test/groovy/com/teradata/test/internal/configuration/YamlConfigurationTest.groovy
+++ test-framework-core/src/test/groovy/com/teradata/test/internal/configuration/YamlConfigurationTest.groovy
@@ -11,27 +11,42 @@ class YamlConfigurationTest
 {
   def static final ENV_VARIABLE_KEY = System.getenv().keySet().iterator().next()
 
-  def getTestYaml()
+
+  def replaceEnvVariables()
   {
-    """\
+    setup:
+    def configuration = new YamlConfiguration("""\
 a:
    b:
       c: ala\${${ENV_VARIABLE_KEY}}
       d: ela\${foo}
 x:
    y: 10
-"""
-  }
-
-  def test()
-  {
-    setup:
-    def configuration = new YamlConfiguration(getTestYaml())
+""")
     expect:
     configuration.listKeys() == ['a.b.c', 'a.b.d', 'x.y'] as Set
     configuration.getInt('x.y') == Optional.of(10)
     configuration.getString('a.b.c') == Optional.of('ala' + System.getenv().get(ENV_VARIABLE_KEY))
     configuration.getString('a.b.d') == Optional.of('ela${foo}')
     configuration.getString('x.y') == Optional.of('10')
+  }
+
+  def replaceSimpleVariables()
+  {
+    setup:
+    def configuration = new YamlConfiguration('''
+items:
+   who: ala
+   verb: ma
+   what: kota
+   what_alias: ${items.what}
+story:
+   ${items.who} ${items.verb} ${items.what}
+story_with_alias:
+   ${items.who} ${items.verb} ${items.what_alias}
+''')
+    expect:
+    configuration.getStringMandatory('story') == "ala ma kota"
+    configuration.getStringMandatory('story_with_alias') == "ala ma kota"
   }
 }
