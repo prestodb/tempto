63f1ddc50afe24b55e2c4ee2ee919c9da8574cd5
Lukasz Osipiuk <lukasz.osipiuk@teradata.com>
2015-09-30 17:19:35 +0200

    AC: Test filtering; safe check for constructor args of TestMetadata

--- tempto-core/src/main/java/com/teradata/tempto/internal/listeners/TestMetadata.java
+++ tempto-core/src/main/java/com/teradata/tempto/internal/listeners/TestMetadata.java
@@ -16,13 +16,16 @@ package com.teradata.tempto.internal.listeners;
 
 import java.util.Set;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.collect.ImmutableSet.copyOf;
+
 public class TestMetadata
 {
     public final Set<String> testGroups;
     public final String testName;
 
     public TestMetadata(Set<String> testGroups, String testName) {
-        this.testGroups = testGroups;
-        this.testName = testName;
+        this.testGroups = copyOf(checkNotNull(testGroups, "testGroups can not be null"));
+        this.testName = checkNotNull(testName, "testName can not be null");
     }
 }
