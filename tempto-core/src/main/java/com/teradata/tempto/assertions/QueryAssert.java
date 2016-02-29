a8ac8ca5b90a1da68bc663afa81686f9b79e0e3c
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2016-02-29 08:38:38 +0200

    Add host mapping functionality
    When doing test with hadoop cluster it may happen that hadoop ports are
    forwarded to different machine. In such case namenode may return address
    of datanode which is unresolveable from localhost. In such case host
    mapping can be helpful.
    
    See HostNameMappingTest and tempto-configuration files for usage.
    
    Test Plan: running tempto examples
    
    Reviewers: losipiuk
    
    Pull request: https://github.com/prestodb/tempto/pull/98

--- tempto-core/src/main/java/com/teradata/tempto/assertions/QueryAssert.java
+++ tempto-core/src/main/java/com/teradata/tempto/assertions/QueryAssert.java
@@ -31,7 +31,6 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static com.google.common.collect.Lists.newArrayList;
