3ed3a5a436c89ba27cc765f16ff099b0ab9d8a58
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2017-08-21 21:19:04 +0200

    Fix stats for tpch.lineitem and tpch.partsupp

---
+++ tempto-examples/src/main/resources/sql-tests/testcases/tpch/show_stats_for_lineitem.sql
@@ -0,0 +1,22 @@
+-- database: presto; groups: stats, tpcds; tables: lineitem
+-- delimiter: |; ignoreOrder: true; types: LONGNVARCHAR|JAVA_OBJECT|DOUBLE|DOUBLE|DOUBLE
+--! name: show stats for lineitem
+SHOW STATS FOR lineitem
+--!
+l_suppkey|null|10000.00000000000|0.00000000000|null|
+l_extendedprice|null|933900.00000000000|0.00000000000|null|
+l_discount|null|11.00000000000|0.00000000000|null|
+l_shipdate|null|null|null|null|
+l_shipinstruct|null|4.00000000000|0.00000000000|null|
+l_quantity|null|50.00000000000|0.00000000000|null|
+l_commitdate|null|null|null|null|
+l_orderkey|null|1500000.00000000000|0.00000000000|null|
+l_partkey|null|200000.00000000000|0.00000000000|null|
+l_receiptdate|null|null|null|null|
+l_comment|null|4580667.00000000000|0.00000000000|null|
+l_linestatus|null|2.00000000000|0.00000000000|null|
+l_shipmode|null|7.00000000000|0.00000000000|null|
+l_returnflag|null|3.00000000000|0.00000000000|null|
+l_tax|null|9.00000000000|0.00000000000|null|
+l_linenumber|null|7.00000000000|0.00000000000|null|
+null|null|null|null|6001215.00000000000|
