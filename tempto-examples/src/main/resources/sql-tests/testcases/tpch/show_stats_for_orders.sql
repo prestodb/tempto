d5fbf501474a829c382a910443011e20ded7f15f
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2017-12-19 12:30:39 +0100

    Use prestodb docker images

--- tempto-examples/src/main/resources/sql-tests/testcases/tpch/show_stats_for_orders.sql
+++ tempto-examples/src/main/resources/sql-tests/testcases/tpch/show_stats_for_orders.sql
@@ -3,13 +3,13 @@
 --! name: show stats for orders 
 SHOW STATS FOR orders
 --!
-o_orderpriority|null|5.0|0.0|null|
-o_orderstatus|null|3.0|0.0|null|
-o_orderkey|null|1500000.0|0.0|null|
-o_custkey|null|99996.0|0.0|null|
-o_totalprice|null|1464556.0|0.0|null|
-o_shippriority|null|1.0|0.0|null|
-o_comment|null|1482071.0|0.0|null|
-o_clerk|null|1000.0|0.0|null|
-o_orderdate|null|null|null|null|
-null|null|null|null|1500000.0|
+o_custkey|null|99996.00000000000|0.00000000000|null|
+o_clerk|null|1000.00000000000|0.00000000000|null|
+o_orderdate|null|2406.00000000000|0.00000000000|null|
+o_shippriority|null|1.00000000000|0.00000000000|null|
+o_totalprice|null|1464556.00000000000|0.00000000000|null|
+o_orderkey|null|1500000.00000000000|0.00000000000|null|
+o_orderstatus|null|3.00000000000|0.00000000000|null|
+o_orderpriority|null|5.00000000000|0.00000000000|null|
+o_comment|null|1482071.00000000000|0.00000000000|null|
+null|null|null|null|1500000.00000000000|
--- tempto-examples/src/main/resources/sql-tests/testcases/tpch/show_stats_for_partsupp.sql
+++ tempto-examples/src/main/resources/sql-tests/testcases/tpch/show_stats_for_partsupp.sql
@@ -3,9 +3,9 @@
 --! name: show stats for partsupp
 SHOW STATS FOR partsupp
 --!
-ps_suppkey|null|10000.00000000000|0.00000000000|null|
+ps_availqty|null|9999.00000000000|0.00000000000|null|
 ps_supplycost|null|99865.00000000000|0.00000000000|null|
 ps_partkey|null|200000.00000000000|0.00000000000|null|
+ps_suppkey|null|10000.00000000000|0.00000000000|null|
 ps_comment|null|799124.00000000000|0.00000000000|null|
-ps_availqty|null|9999.00000000000|0.00000000000|null|
 null|null|null|null|800000.00000000000|
