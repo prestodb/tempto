prestodb/tempto

commit b82f9ad6f5163ca878114d8dea65cc556dfe14f0

Make possible to pass multiple files to --config option

tempto-examples/bin/run_on_docker.sh
@@ -41,7 +41,7 @@ function check_presto() {
 function run_product_tests() {
   ${DOCKER_COMPOSE} run --rm -T runner \
     java -jar /workspace/build/libs/tempto-examples-all.jar \
-    --config-local /workspace/docker/tempto-configuration-docker-local.yaml \
+    --config tempto-configuration.yaml,/workspace/docker/tempto-configuration-docker-local.yaml \
     $*
 }
 

tempto-core/src/main/java/com/teradata/tempto/internal/configuration/TestConfigurationFactory.java
@@ -13,6 +13,7 @@
  */
 package com.teradata.tempto.internal.configuration;
 
+import com.google.common.base.Splitter;
 import com.teradata.tempto.configuration.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -56,12 +57,17 @@ public class TestConfigurationFactory
 
     private static Configuration readTestConfiguration()
     {
-        String testConfigurationUri = System.getProperty(TEST_CONFIGURATION_URI_KEY, DEFAULT_TEST_CONFIGURATION_LOCATION);
-        Optional<InputStream> testConfigurationStream = getConfigurationInputStream(testConfigurationUri);
-        if (!testConfigurationStream.isPresent()) {
-            throw new IllegalArgumentException("Unable find to configuration: " + testConfigurationUri);
+        String testConfigurationUris = System.getProperty(TEST_CONFIGURATION_URI_KEY, DEFAULT_TEST_CONFIGURATION_LOCATION);
+        Configuration configuration = emptyConfiguration();
+        for (String testConfigurationUri : Splitter.on(",").split(testConfigurationUris)) {
+            Optional<InputStream> testConfigurationStream = getConfigurationInputStream(testConfigurationUri);
+            if (!testConfigurationStream.isPresent()) {
+                throw new IllegalArgumentException("Unable find to configuration: " + testConfigurationUri);
+            }
+            Configuration parsedConfiguration = parseConfiguration(testConfigurationStream.get());
+            configuration = new HierarchicalConfiguration(configuration, parsedConfiguration);
         }
-        return parseConfiguration(testConfigurationStream.get());
+        return configuration;
     }
 
     private static Configuration readLocalConfiguration()

tempto-core/src/test/groovy/com/teradata/tempto/internal/configuration/TestConfigurationFactoryTest.groovy
@@ -42,4 +42,22 @@ class TestConfigurationFactoryTest
     configuration.getStringMandatory('resolve.both') == 'local'
     configuration.getStringMandatory('resolve.global') == 'global'
   }
+
+  def 'read two test configurations'() {
+    setup:
+    System.setProperty(TEST_CONFIGURATION_URI_KEY, "/configuration/global-configuration-tempto.yaml,/configuration/local-configuration-tempto.yaml");
+
+    when:
+    Configuration configuration = TestConfigurationFactory.createTestConfiguration()
+
+    then:
+    configuration.getStringMandatory('value.local') == 'local'
+    configuration.getStringMandatory('value.both') == 'local'
+    configuration.getStringMandatory('value.global') == 'global'
+    configuration.getStringMandatory('value.default') == 'default_value'
+
+    configuration.getStringMandatory('resolve.local') == 'local'
+    configuration.getStringMandatory('resolve.both') == 'local'
+    configuration.getStringMandatory('resolve.global') == 'global'
+  }
 }

tempto-runner/src/main/java/com/teradata/tempto/runner/TemptoRunnerCommandLineParser.java
@@ -14,6 +14,7 @@
 
 package com.teradata.tempto.runner;
 
+import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.teradata.tempto.internal.configuration.TestConfigurationFactory;

tempto-runner/src/main/java/com/teradata/tempto/runner/TemptoRunnerOptions.java
@@ -38,7 +38,8 @@ public class TemptoRunnerOptions
 
     public static final Option CONFIG_FILE = Option.builder("f")
             .longOpt("config")
-            .desc("Path to main test configuration YAML file. If file does not exists in local file system then classpath is checked")
+            .desc("Path to test configuration YAML files. If file does not exists in local file system then classpath is checked")
+            .valueSeparator(',')
             .hasArg()
             .build();
 

commit c0f9e68cc01b18c5bdaa5cbdac02007f245ae3b9

Override main configuration in tempto-examples
It was broken in b82f9ad6f5163ca878114d8dea65cc556dfe14f0.
.travis.yml was changing the main (first) configuration file so it
tempto-examples were testing if it is possible to run tempto with
minimal configuration (that it is only used by tests that were
executed).

This commits restores that.

.travis.yml
@@ -24,6 +24,6 @@ install: ./gradlew build -x test
 script:
   - ./gradlew test
   - ./tempto-examples/bin/run_on_docker.sh --thread-count 2 -x failing
-  - ./tempto-examples/bin/run_on_docker.sh --thread-count 2 --config tempto-configuration-read-only.yaml -g in_memory
-  - ./tempto-examples/bin/run_on_docker.sh --thread-count 2 --config tempto-configuration-no-db.yaml -g ssh
-  - ./tempto-examples/bin/run_on_docker.sh --thread-count 2 --config tempto-configuration-invalid-ssh-and-psql.yaml -g in_memory
+  - CONFIG_FILE=tempto-configuration-read-only.yaml ./tempto-examples/bin/run_on_docker.sh --thread-count 2 -g in_memory
+  - CONFIG_FILE=tempto-configuration-no-db.yaml ./tempto-examples/bin/run_on_docker.sh --thread-count 2 -g ssh
+  - CONFIG_FILE=tempto-configuration-invalid-ssh-and-psql.yaml ./tempto-examples/bin/run_on_docker.sh --thread-count 2 -g in_memory

tempto-examples/bin/run_on_docker.sh
@@ -39,9 +39,10 @@ function check_presto() {
 }
 
 function run_product_tests() {
+  CONFIG_FILE=${CONFIG_FILE:-tempto-configuration.yaml}
   ${DOCKER_COMPOSE} run --rm -T runner \
     java -jar /workspace/build/libs/tempto-examples-all.jar \
-    --config tempto-configuration.yaml,/workspace/docker/tempto-configuration-docker-local.yaml \
+    --config ${CONFIG_FILE},/workspace/docker/tempto-configuration-docker-local.yaml \
     $*
 }
 