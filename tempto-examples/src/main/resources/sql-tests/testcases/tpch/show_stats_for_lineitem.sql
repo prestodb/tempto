b66d202bcc48426ae9fad8bf42059d5f4e8a712c
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2017-12-19 12:30:39 +0100

    Use prestodb docker images

--- tempto-examples/src/main/resources/sql-tests/testcases/tpch/show_stats_for_lineitem.sql
+++ tempto-examples/src/main/resources/sql-tests/testcases/tpch/show_stats_for_lineitem.sql
@@ -3,20 +3,20 @@
 --! name: show stats for lineitem
 SHOW STATS FOR lineitem
 --!
-l_suppkey|null|10000.00000000000|0.00000000000|null|
 l_extendedprice|null|933900.00000000000|0.00000000000|null|
-l_discount|null|11.00000000000|0.00000000000|null|
-l_shipdate|null|null|null|null|
-l_shipinstruct|null|4.00000000000|0.00000000000|null|
-l_quantity|null|50.00000000000|0.00000000000|null|
-l_commitdate|null|null|null|null|
 l_orderkey|null|1500000.00000000000|0.00000000000|null|
+l_shipdate|null|2526.00000000000|0.00000000000|null|
+l_commitdate|null|2466.00000000000|0.00000000000|null|
 l_partkey|null|200000.00000000000|0.00000000000|null|
-l_receiptdate|null|null|null|null|
+l_quantity|null|50.00000000000|0.00000000000|null|
 l_comment|null|4580667.00000000000|0.00000000000|null|
-l_linestatus|null|2.00000000000|0.00000000000|null|
-l_shipmode|null|7.00000000000|0.00000000000|null|
+l_receiptdate|null|2554.00000000000|0.00000000000|null|
 l_returnflag|null|3.00000000000|0.00000000000|null|
 l_tax|null|9.00000000000|0.00000000000|null|
+l_shipinstruct|null|4.00000000000|0.00000000000|null|
 l_linenumber|null|7.00000000000|0.00000000000|null|
+l_suppkey|null|10000.00000000000|0.00000000000|null|
+l_discount|null|11.00000000000|0.00000000000|null|
+l_linestatus|null|2.00000000000|0.00000000000|null|
+l_shipmode|null|7.00000000000|0.00000000000|null|
 null|null|null|null|6001215.00000000000|
