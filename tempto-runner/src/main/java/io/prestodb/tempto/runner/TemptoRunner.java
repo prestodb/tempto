b931732e5de49c3e9454161010463d8ca0fd9d11
Anant Aneja <1797669+aaneja@users.noreply.github.com>
2024-10-14 14:41:47 +0530

    Migrate from NameServiceDescriptor/NameService to InetAddressResolverProvider/InetAddressResolver

--- tempto-runner/src/main/java/io/prestodb/tempto/runner/TemptoRunner.java
+++ tempto-runner/src/main/java/io/prestodb/tempto/runner/TemptoRunner.java
@@ -15,7 +15,7 @@
 package io.prestodb.tempto.runner;
 
 import com.google.common.base.Joiner;
-import io.prestodb.tempto.dns.TemptoNameServiceDescriptor;
+import io.prestodb.tempto.dns.TemptoInetAddressResolverProvider;
 import io.prestodb.tempto.internal.listeners.TestNameGroupNameMethodSelector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,7 +39,7 @@ import static java.util.Collections.singletonList;
 public class TemptoRunner
 {
     static {
-        TemptoNameServiceDescriptor.enableHostMapping();
+        TemptoInetAddressResolverProvider.enableHostMapping();
     }
 
     private static final Logger LOG = LoggerFactory.getLogger(TemptoRunner.class);
